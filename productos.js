e086344e3777bb751b5c4f47d96b4d6c1f695a9b
ManuelaE <manuelaepedesign@gmail.com>
2018-10-30 21:44:43 -0500

    message

---
+++ productos.js
@@ -0,0 +1,399 @@
+[
+    {
+        img: "/imagenes/7.png",
+        titulo: "Collar Invierno",
+        precio: 66.000, 
+        talla: [
+             "S (230mm-280mm)", 
+             "M (300mm-350mm)",
+             "L (370mm-420mm)"
+             ],
+        color: "azul",
+        colori: "",
+        descripcion: "Un hermoso collar que represente la personalidad de tu mascota.",
+        filtro: "collar"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Collar Fiesta",
+        precio: 63.000, 
+        talla: [
+             "S (230mm-280mm)", 
+             "M (300mm-350mm)",
+             "L (370mm-420mm)"
+             ],
+        color: "amarillo",
+        colori: "",
+        descripcion: "Un hermoso collar que represente la personalidad de tu mascota.",
+        filtro: "collar"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Collar Bambú",
+        precio: 60.000, 
+        talla: [
+             "S (230mm-280mm)", 
+             "M (300mm-350mm)",
+             "L (370mm-420mm)"
+             ],
+        color: "verde",
+        colori: "",
+        descripcion: "Un hermoso collar que represente la personalidad de tu mascota.",
+        filtro: "collar"  
+    },
+    {
+       img: "/imagenes/7.png",
+       titulo: "Collar Carnaval",
+       precio: 52.000, 
+       talla: [
+            "S (230mm-280mm)", 
+            "M (300mm-350mm)",
+            "L (370mm-420mm)"
+            ],
+       color: "rojo",
+       colori: "",
+       colori: "",
+       descripcion: "Un hermoso collar que represente la personalidad de tu mascota.",
+       filtro: "collar"  
+    },
+    {
+        img: "/imagenes/6.png",
+        titulo: "Collar Atlántico",
+        precio: 63.000, 
+        talla: [
+             "S (230mm-280mm)", 
+             "M (300mm-350mm)",
+             "L (370mm-420mm)"
+             ],
+        color: "azul",
+        colori: "",
+        descripcion: "Un hermoso collar que represente la personalidad de tu mascota.",
+        filtro: "collar"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Collar Verano",
+        precio: 66.000, 
+        talla: [
+             "S (230mm-280mm)", 
+             "M (300mm-350mm)",
+             "L (370mm-420mm)"
+             ],
+        color: "verde",
+        colori: "",
+        descripcion: "Un hermoso collar que represente la personalidad de tu mascota.",
+        filtro: "collar"  
+    },
+    {
+        img: "/imagenes/8.png",
+        titulo: "Collar Primavera",
+        precio: 58.000, 
+        talla: [
+             "S (230mm-280mm)", 
+             "M (300mm-350mm)",
+             "L (370mm-420mm)"
+             ],
+        color: "otros",
+        colori: "",
+        descripcion: "Un hermoso collar que represente la personalidad de tu mascota.",
+        filtro: "collar"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Collar Nevado",
+        precio: 63.000, 
+        talla: [
+             "S (230mm-280mm)", 
+             "M (300mm-350mm)",
+             "L (370mm-420mm)"
+             ],
+        color: "azul",
+        colori: "",
+        descripcion: "Un hermoso collar que represente la personalidad de tu mascota.",
+        filtro: "collar"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Arnés Atlántico",
+        precio: 72.000, 
+        talla: [
+            "S (280mm-380mm)", 
+            "M (300mm-440mm)",
+            "L (330mm-550mm)"
+             ],
+        color: "azul",
+        colori: "",
+        descripcion: "Este arnés es el detalle perfecto para llenar de estilo a tu mascota.",
+        filtro: "arnes"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Arnés Fiesta",
+        precio: 70.000, 
+        talla: [
+             "S (280mm-380mm)", 
+             "M (300mm-440mm)",
+             "L (330mm-550mm)"
+             ],
+        color: "amarillo",
+        colori: "",
+        descripcion: "Este arnés es el detalle perfecto para llenar de estilo a tu mascota.",
+        filtro: "arnes"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Arnés Bambú",
+        precio: 74.000, 
+        talla: [
+            "S (280mm-380mm)", 
+            "M (300mm-440mm)",
+            "L (330mm-550mm)"
+             ],
+        color: "verde",
+        colori: "",
+        descripcion: "Este arnés es el detalle perfecto para llenar de estilo a tu mascota.",
+        filtro: "arnes"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Arnés Carnaval",
+        precio: 72.000, 
+        talla: [
+            "S (280mm-380mm)", 
+            "M (300mm-440mm)",
+            "L (330mm-550mm)"
+             ],
+        color: "rojo",
+        colori: "",
+        descripcion: "Este arnés es el detalle perfecto para llenar de estilo a tu mascota.",
+        filtro: "arnes"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Correa Invierno",
+        precio: 66.000, 
+        talla: [
+             "S (1600mm)", 
+             "M (2100mm)",
+             "L (2200mm)"
+             ],
+        color: "azul",
+        colori: "",
+        descripcion: "Un toque de sofisticación para que tu mascota luzca perfecta.",
+        filtro: "correa"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Correa Fiesta",
+        precio: 66.000, 
+        talla: [
+            "S (1600mm)", 
+            "M (2100mm)",
+            "L (2200mm)"
+             ],
+        color: "amarillo",
+        colori: "",
+        descripcion: "Un toque de sofisticación para que tu mascota luzca perfecta.",
+        filtro: "correa"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Correa Bambú",
+        precio: 70.000, 
+        talla: [
+            "S (1600mm)", 
+            "M (2100mm)",
+            "L (2200mm)"
+             ],
+        color: "verde",
+        colori: "",
+        descripcion: "Un toque de sofisticación para que tu mascota luzca perfecta.",
+        filtro: "correa"  
+    },
+    {
+       img: "/imagenes/7.png",
+       titulo: "Correa Carnaval",
+       precio: 70.000, 
+       talla: [
+            "S (1600mm)", 
+            "M (2100mm)",
+            "L (2200mm)"
+            ],
+       color: "rojo",
+       colori: "",
+       descripcion: "Un toque de sofisticación para que tu mascota luzca perfecta.",
+       filtro: "correa"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Correa Atlántico",
+        precio: 58.000, 
+        talla: [
+            "S (1600mm)", 
+            "M (2100mm)",
+            "L (2200mm)"
+             ],
+        color: "azul",
+        colori: "",
+        descripcion: "Un toque de sofisticación para que tu mascota luzca perfecta.",
+        filtro: "correa"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Correa Verano",
+        precio: 58.000, 
+        talla: [
+            "S (1600mm)", 
+            "M (2100mm)",
+            "L (2200mm)"
+             ],
+        color: "verde",
+        colori: "",
+        descripcion: "Un toque de sofisticación para que tu mascota luzca perfecta.",
+        filtro: "correa"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Correa Primavera",
+        precio: 66.000, 
+        talla: [
+            "S (1600mm)", 
+            "M (2100mm)",
+            "L (2200mm)"
+             ],
+        color: "otros",
+        colori: "",
+        descripcion: "Un toque de sofisticación para que tu mascota luzca perfecta.",
+        filtro: "correa"  
+    },
+    {
+        img: "/imagenes/7.png",
+        titulo: "Correa Nevado",
+        precio: 66.000, 
+        talla: [
+            "S (1600mm)", 
+            "M (2100mm)",
+            "L (2200mm)"
+             ],
+        color: "azul",
+        colori: "",
+        descripcion: "Un toque de sofisticación para que tu mascota luzca perfecta.",
+        filtro: "correa"  
+    },
+    {
+        img: "/imagenes/1.png",
+        titulo: "Cama Oceanía",
+        precio: 255.000, 
+        talla: [
+            "S (630*500*440)", 
+            "L (750*580*500)"
+             ],
+        color: "azul",
+        colori: "",
+        descripcion: "Esta cama es ideal para que tu mascota se relaje y descanse.",
+        filtro: "cama"  
+    },
+    {
+        img: "/imagenes/2.png",
+        titulo: "Cama Asiática",
+        precio: 255.000, 
+        talla: [
+            "S (630*500*440)", 
+            "L (750*580*500)"
+             ],
+        color: "beige",
+        colori: "",
+        descripcion: "Esta cama es ideal para que tu mascota se relaje y descanse.",
+        filtro: "cama"  
+    },
+    {
+        img: "/imagenes/3.png",
+        titulo: "Cama Europea",
+        precio: 255.000, 
+        talla: [
+            "S (630*500*440)", 
+            "L (750*580*500)"
+             ],
+        color: "rosa",
+        colori: "",
+        descripcion: "Esta cama es ideal para que tu mascota se relaje y descanse.",
+        filtro: "cama"  
+    },
+    {
+        img: "/imagenes/4.png",
+        titulo: "Cama Americana",
+        precio: 255.000, 
+        talla: [
+            "S (630*500*440)", 
+            "L (750*580*500)"
+             ],
+        color: "azul",
+        colori: "",
+        descripcion: "Esta cama es ideal para que tu mascota se relaje y descanse.",
+        filtro: "cama"  
+    },
+    {
+        img: "/imagenes/10.png",
+        titulo: "Buso Vino",
+        precio: 74.000, 
+        talla: [
+            "1 (1,5-2,5 kg)", 
+            "2 (2,5-3,5 kg)",
+            "3 (3,5-5,5 kg)",
+            "4 (5-8 kg)",
+            "5 (8-11 kg)"
+             ],
+        color: "otros",
+        colori: "",
+        descripcion: "Este buso es ideal para proteger a tu mascota en los días fríos.",
+        filtro: "buso"  
+    },
+    {
+        img: "/imagenes/12.png",
+        titulo: "Buso Mate",
+        precio: 74.000, 
+        talla: [
+            "1 (1,5-2,5 kg)", 
+            "2 (2,5-3,5 kg)",
+            "3 (3,5-5,5 kg)",
+            "4 (5-8 kg)",
+            "5 (8-11 kg)"
+             ],
+        color: "otros",
+        colori: "",
+        descripcion: "Este buso es ideal para proteger a tu mascota en los días fríos.",
+        filtro: "buso"  
+    },
+    {
+        img: "/imagenes/11.png",
+        titulo: "Buso Aqua",
+        precio: 94.000, 
+        talla: [
+            "1 (1,5-2,5 kg)", 
+            "2 (2,5-3,5 kg)",
+            "3 (3,5-5,5 kg)",
+            "4 (5-8 kg)",
+            "5 (8-11 kg)"
+             ],
+        color: "azul",
+        colori: "",
+        descripcion: "Este buso es ideal para proteger a tu mascota en los días fríos.",
+        filtro: "buso"  
+    },
+    {
+        img: "/imagenes/9.png",
+        titulo: "Buso Camel",
+        precio: 94.000, 
+        talla: [
+            "1 (1,5-2,5 kg)", 
+            "2 (2,5-3,5 kg)",
+            "3 (3,5-5,5 kg)",
+            "4 (5-8 kg)",
+            "5 (8-11 kg)"
+             ],
+        color: "amarillo",
+        colori: "",
+        descripcion: "Este buso es ideal para proteger a tu mascota en los días fríos.",
+        filtro: "buso"  
+    },
+]
