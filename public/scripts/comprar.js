a19da9a09336c88c6bee77fa48e9555d0bce8341
ManuelaE <manuelaepedesign@gmail.com>
2018-11-02 00:28:27 -0500

    12:28 no pierdo la fe

--- public/scripts/comprar.js
+++ public/scripts/comprar.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function(){
 
     var total = 0;
-    var precios = document.querySelectorAll('.compra__carro__prop__precio');
+    var precios = document.querySelectorAll('.compra__carro__prop__fn__precio');
 
     precios.forEach(function (elem){
         var x = parseInt(elem.innerHTML);
